f4b6092887d513080cd7d59c841d95cf8288a80f
Kensuke Takeda <takebbb1@gmail.com>
2018-01-22 01:56:06 +0000

    再チャレンジ中です

---
+++ scripts/hello.js
@@ -0,0 +1,8 @@
+'use strict';
+module.exports = (robot) => {
+    robot.hear(/hello>/i, (msg) => {
+        const username = msg.message.user.name;
+        msg.send('Hello, ' + username);
+
+    });
+};
